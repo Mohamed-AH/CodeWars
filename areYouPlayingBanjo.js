c1658374454559c480468142c61376365d65ab02
Mohamed-AH <emah84@gmail.com>
2022-03-19 08:00:37 +0300

    Create areYouPlayingBanjo.js

---
+++ areYouPlayingBanjo.js
@@ -0,0 +1,8 @@
+function areYouPlayingBanjo(name) {
+  // Implement me
+  if (name[0] == "r" || name[0] == "R") {
+    return name + " plays banjo";
+  } else {
+    return name + " does not play banjo";
+  }
+}
