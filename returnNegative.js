75f7ca7de6996d153654dbe8496a451fae8354b1
Mohamed <emah84@gmail.com>
2022-03-24 09:29:13 +0300

    oppositeNumber returnNegative strRepeat

---
+++ returnNegative.js
@@ -0,0 +1,5 @@
+function makeNegative(num) {
+  if (num <= 0) {
+    return num;
+  } else return num * -1;
+}
---
+++ strRepeat.js
@@ -0,0 +1,5 @@
+function repeatStr(n, s) {
+  let t = "";
+  for (i = 0; i < n; i++) t += s;
+  return t;
+}
