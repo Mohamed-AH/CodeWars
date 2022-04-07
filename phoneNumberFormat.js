1c9dd9638d4bebd68fbc87bfdc1eb5499d86cf3f
Mohamed-AH <emah84@gmail.com>
2022-04-07 16:19:47 +0300

    1 6kyu

---
+++ phoneNumberFormat.js
@@ -0,0 +1,9 @@
+function createPhoneNumber(numbers) {
+  let phoneNumberString = numbers.join("");
+  var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return "(" + match[1] + ") " + match[2] + "-" + match[3];
+  }
+  return null;
+}
