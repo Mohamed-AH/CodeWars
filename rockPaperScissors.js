30e816130cfcb83cf9c19ebbd7a578255027fd86
Mohamed-AH <emah84@gmail.com>
2022-04-18 16:53:46 +0300

    1 8kyu

---
+++ rockPaperScissors.js
@@ -0,0 +1,13 @@
+const rps = (p1, p2) => {
+  if (p1 == p2) {
+    return "Draw!";
+  } else if (
+    (p1 == "scissors" && p2 == "paper") ||
+    (p1 == "rock" && p2 == "scissors") ||
+    (p1 == "paper" && p2 == "rock")
+  ) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+};
