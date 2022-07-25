2ffc60e4bcff03a219f10db19e37b2c3f4cdf0d2
Mohamed-AH <emah84@gmail.com>
2022-07-25 09:27:29 +0300

    8kyus

---
+++ validateUsrRegx.js
@@ -0,0 +1,14 @@
+function validateUsr(username) {
+  /**
+    - `^`        Start from the beginning of the string.
+    - `[]`       Allow any character specified, including...
+    - `a-z`      anything from a to z,
+    - `0-9`      anything from 0 to 9,
+    - `_`        and underscore.
+    - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+    - `$`        End the string right after specified amount of allowed characters is given.
+  */
+  const validator = /^[a-z0-9_]{4,16}$/;
+  
+  return validator.test(username);
+}
