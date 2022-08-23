de2a0c948c08caec73b333cb4a58fc41b7228487
Mohamed-AH <emah84@gmail.com>
2022-08-23 09:39:53 +0300

    8kyus

---
+++ combineName.js
@@ -0,0 +1,6 @@
+function combineNames(a1,a2)
+{  
+  return a1 + " " + a2
+}
+// Create the combineNames function here
+//chunk(a1,a2)
---
+++ removeRev03.js
@@ -0,0 +1,4 @@
+function remove (string) {
+  //coding and coding....
+  return string.split('!').join("")+'!';  
+}
